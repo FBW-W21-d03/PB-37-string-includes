1fa3b6d8778abffcd2912c56397601b460bdbbbf
slavablack <veaceslav.ciornii@magenta.de>
2022-01-03 11:21:07 +0100

    Veaceslav

---
+++ index.js
@@ -0,0 +1,8 @@
+//1
+let zitat= ("Ich habe mir immer gewünscht, dass mein Computer so leicht zu bedienen ist wie mein Telefon; mein Wunsch ging in Erfüllung: mein Telefon kann ich jetzt auch nicht mehr bedienen.");
+//2
+let check = zitat.includes("Telefon");
+console.log(check);
+//3
+let check2 = zitat.includes("Stroustrup");
+console.log(check2);
