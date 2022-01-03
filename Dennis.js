4dc90f33c713923f1bd0d5c44bd5891245357366
Dennisbullys <d.rochus@hotmail.de>
2022-01-03 11:32:31 +0100

    Dennis

---
+++ Dennis.js
@@ -0,0 +1,20 @@
+// Aufgabe 1
+
+
+let articel = "Ich habe mir immer gewünscht, dass mein Computer so leicht zu bedienen ist wie mein Telefon; mein Wunsch ging in Erfüllung: mein Telefon kann ich jetzt auch nicht mehr bedienen.";
+
+
+// Aufgabe 2
+
+let isIncluded = articel.includes("Telefon");
+
+console.log(isIncluded);
+
+
+// Aufgabe 3
+let isIncluded1 = articel.includes("Stroustrup");
+
+console.log(isIncluded1);
+
+  
+
