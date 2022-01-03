afad08460a903be6589d66d25f333873c40b7953
sham912411 <shamalhakim91@gmail.com>
2022-01-03 11:49:04 +0100

    lösung

---
+++ script.js
@@ -0,0 +1,12 @@
+let zitat = "Ich habe mir immer gewünscht, dass mein Computer so leicht zu bedienen ist wie mein Telefon; mein Wunsch ging in Erfüllung: mein Telefon kann ich jetzt auch nicht mehr bedienen.
+
+";
+
+
+let isZitat = zitat.includes("Telefon;")
+console.log(isZitat)
+
+
+
+let isZitat = zitat.includes("Stroustrup")
+console.log(isZitat)
