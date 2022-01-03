d2e252ed1b588994519aac64a3229fbec3c95ca1
Hayat Alkheder <hayat.alkheder@gmail.com>
2022-01-03 11:24:41 +0100

    js

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1)
+let article = "Ich habe mir immer gewünscht, dass mein Computer so leicht zu bedienen ist wie mein Telefon; mein Wunsch ging in Erfüllung: mein Telefon kann ich jetzt auch nicht mehr bedienen. "
+// 2)
+let isIncluded = article.includes("Telefon");
+
+console.log(isIncluded);
+// 3)
+let included = article.includes("Stroustrup");
+
+console.log(included);
