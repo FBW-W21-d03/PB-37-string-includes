878deb3d960aed2fd638db382516d8b8984a4d94
ShiinyFlakes <mago_elm@hotmail.com>
2022-01-03 16:15:29 +0100

    Mago

---
+++ script.js
@@ -0,0 +1,7 @@
+let zitat = "Ich habe mir immer gewünscht, dass mein Computer so leicht zu bedienen ist wie mein Telefon; mein Wunsch ging in Erfüllung: mein Telefon kann ich jetzt auch nicht mehr bedienen.";
+
+function check (searchWord) {
+    console.log(zitat.includes(searchWord))
+}
+
+check("Stroustrup");
