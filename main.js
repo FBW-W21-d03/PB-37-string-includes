dd62e7342bc0b009bfdf4b63b107b21c1d2f3f2f
kawanaza <kawanbabani@gmail.com>
2022-01-03 11:30:17 +0100

    done

---
+++ main.js
@@ -0,0 +1,11 @@
+let Zitat =
+  "ch habe mir immer gewünscht, dass mein Computer so leicht zu bedienen ist wie mein Telefon; mein Wunsch ging in Erfüllung: mein Telefon kann ich jetzt auch nicht mehr bedienen.";
+
+/// Verwende die Methode includes(), um zu prüfen, ob der Begriff "Telefon" enthalten ist.//
+let telefon = Zitat.includes("Telefon");
+console.log(telefon);
+
+///Verwende die Methode includes(), um zu prüfen, ob der Begriff "Stroustrup" enthalten ist.//
+
+let stro = Zitat.includes("Stroustrup");
+console.log(stro);
