adb6e08a6c60ee3f0c16f3b1902c6f323859f781
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-01-03 11:20:15 +0100

    Orhan's lösung

---
+++ string-includes.js
@@ -0,0 +1,11 @@
+// Aufgabe 1
+let Zitat = "Ich habe mir immer gewünscht, dass mein Computer so leicht zu bedienen ist wie mein Telefon; mein Wunsch ging in Erfüllung: mein Telefon kann ich jetzt auch nicht mehr bedienen."
+
+// Aufgabe 2
+let isIncluded = Zitat.includes("Telefon");
+console.log(isIncluded);
+
+
+// Aufgabe 3
+let isIncluded2 = Zitat.includes("Stroustrup");
+console.log(isIncluded2);
