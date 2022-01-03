220286ecfe17be1a2c3a55062f4d43a072f2f6b8
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-03 11:29:01 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,10 @@
+// AUFGABE TEIL 1:
+const zitat ="Ich habe mir immer gewünscht, dass mein Computer so leicht zu bedienen ist wie mein Telefon; mein Wunsch ging in Erfüllung: mein Telefon kann ich jetzt auch nicht mehr bedienen.";
+
+// AUFGABE TEIL 2:
+let isIncluded = zitat.includes("Telefon");
+console.log(isIncluded);
+
+// AUFGABE TEIL 3:
+let isIncluded1 = zitat.includes("Stroustrup");
+console.log(isIncluded1)
