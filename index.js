3f0fdcc86ea0ce17b2c6b05fcc447539a92345e9
Mohal1125 <rajwindersinghmohal@yahoo.com>
2022-01-03 11:23:14 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,9 @@
+let BjarneStroustrup = "Ich habe mir immer gewünscht, dass mein Computer so leicht zu bedienen ist wie mein Telefon; mein Wunsch ging in Erfüllung: mein Telefon kann ich jetzt auch nicht mehr bedienen.";
+
+let isIncluded = BjarneStroustrup.includes("Telefon");
+
+console.log(isIncluded);
+
+isIncluded = BjarneStroustrup.includes("Stroustrup");
+
+console.log(isIncluded);
