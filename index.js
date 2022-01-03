5e6faa6a275ee8f12a51b6aedb68941cd9dd495a
Mazen <mazen.taha88@gmail.com>
2022-01-03 11:24:08 +0100

    My Lösung

---
+++ index.js
@@ -0,0 +1,9 @@
+let Bjarne = "Ich habe mir immer gewünscht, dass mein Computer so leicht zu bedienen ist wie mein Telefon; mein Wunsch ging in Erfüllung: mein Telefon kann ich jetzt auch nicht mehr bedienen."
+
+
+let isIncluded = Bjarne.includes("Telefon");
+console.log(isIncluded);
+
+
+let isIncludedStroustrup = Bjarne.includes("stroustrup");
+console.log(isIncludedStroustrup);
