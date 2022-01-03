ce97a39b6f42bb74080c9e722ea124122e2cc21a
sprotte <metatron1972@live.de>
2022-01-03 11:26:27 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,7 @@
+let article = "Ich habe mir immer gewünscht, dass mein Computer so leicht zu bedienen ist wie mein Telefon; mein Wunsch ging in Erfüllung: mein Telefon kann ich jetzt auch nicht mehr bedienen. "
+
+let isIncluded = article.includes("Telefon");
+
+/* let isIncluded = article.includes("Stroustrup"); */
+
+console.log(isIncluded);
