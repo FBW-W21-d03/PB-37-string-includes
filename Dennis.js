df94fb7646a96fe2c480fa3ce939531591285ba1
Dennisbullys <d.rochus@hotmail.de>
2022-01-04 09:06:22 +0100

    Dennis

--- Dennis.js
+++ Dennis.js
@@ -16,5 +16,16 @@ let isIncluded1 = articel.includes("Stroustrup");
 
 console.log(isIncluded1);
 
-  
 
+// Aufgabe 4
+
+
+let zitat = "Ich habe mir immer gewünscht, dass mein Computer so leicht zu bedienen ist wie mein Telefon; mein Wunsch ging in Erfüllung: mein Telefon kann ich jetzt auch nicht mehr bedienen.";
+
+ 
+function check(searchWord) {
+  let included2 = zitat.includes(searchWord);
+  console.log("Suchergebnis:", included2);
+}
+check("Telefon");
+check("Stroustrup");
